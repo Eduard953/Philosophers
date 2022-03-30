1d3636a58975db08ba6e4990a7f2acd35149ed7b
Eduard Beiline <eduardbeiline@Eduards-Air.local>
2022-03-30 15:11:30 +0200

    added memory manageent for philo struct

--- clean.c
+++ clean.c
@@ -6,7 +6,7 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 14:51:36 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/03/30 14:45:40 by ebeiline         ###   ########.fr       */
+/*   Updated: 2022/03/30 15:10:51 by ebeiline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	clean(t_vars *vars)
 		while (++i < vars->num_philo)
 			pthread_mutex_destroy(&vars->forks[i]);
 		free(vars->forks);
+		free(vars->phils);
 	}
 //	pthread_mutex_unlock(&vars->access);
 	pthread_mutex_destroy(&vars->access);
